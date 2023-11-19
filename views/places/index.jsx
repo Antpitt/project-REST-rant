960b291482bcbbf244ef4da335866b81ed66590d
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2023-11-19 11:41:41 -0800

    made index page

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('../default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1>Index Page</h1>
+              <p>Yes you made it to the index page!</p>
+          </main>
+      </Def>
+    )
+  }
+  
+module.exports = index
