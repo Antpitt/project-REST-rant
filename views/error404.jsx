abff381f09fc5284d2751802effd1aa7b91d23e2
pittman.anthony17@gmail.com <pittman.anthony17@gmail.com>
2023-11-28 16:52:22 -0800

    images

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+              <img src="./images/puppy.jpg" alt="Cute puppy"/>
+              </div>
+              Photo by <a href="https://unsplash.com/@priscilladupreez?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Priscilla Du Preez 🇨🇦</a> on <a href="https://unsplash.com/photos/black-pug-puppy-on-green-textile-SIgxB-9GdR8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -3,15 +3,19 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-                <button className="btn-primary">Places Page</button>
-              </a>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="../images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                </div>
+                Photo By <a href="https://unsplash.com/@seefromthesky">Ishan</a> on <a href="https://unsplash.com/">Unsplash</a>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
+
 module.exports = home
